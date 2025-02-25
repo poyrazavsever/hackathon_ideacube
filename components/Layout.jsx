d7cf7c41d31faae92f7b17ef13bd41943e21b1c9
Poyraz Avsever <poyrazavsever@hotmail.com>
2025-02-25 08:14:04 +0300

    feat: installed react-hot-toast n fixed env file in firebase

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { Toaster } from "react-hot-toast"; // React Hot Toast import
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main>
-        {children}
-      </main>
+      <main className="flex-grow">{children}</main>
       <Footer />
+      {/* Toaster component added here */}
+      <Toaster
+        position="bottom-center"
+        reverseOrder={false}
+        toastOptions={{
+          className: "bg-purple-600 text-white p-4 rounded-lg shadow-lg",
+          duration: 4000, // Toast gösterim süresi
+        }}
+      />
     </div>
   );
 };
